edd1e64699c7f5a0b7451b75d21508148a582db6
Sam Zhang <shimian.zhang@gmail.com>
2023-03-29 14:29:52 -0400

    csv instead of parquet input

--- import/src/make_CO_strata.R
+++ import/src/make_CO_strata.R
@@ -1,16 +1,13 @@
 library(pacman)
-pacman::p_load(
-   here, yaml, dplyr,
-   arrow # for reading parquet file
-)
+pacman::p_load(here, yaml, tidyverse)
 
 co_yaml_path <- here("import", "hand", "CO.yaml")
-co_raw_path <- here("import", "input", "mr-homicidio-R1.parquet")
+co_raw_path <- here("import", "input", "mr-homicidio-R1.csv")
 co_dept_groups_path <- here("import", "input", "dept-groups.csv")
 co_regions_path <- here("import", "input", "regiones_onic.csv")
 
 if (!file.exists(co_raw_path)) {
-    stop("input/mr-homicidio-R1.parquet does not exist")
+    stop("input/mr-homicidio-R1.csv does not exist")
 }
 if (!file.exists(co_dept_groups_path)) {
     stop("input/dept-groups.csv does not exist")
@@ -21,7 +18,7 @@ if (!file.exists(co_regions_path)) {
 
 strata_spec <- read_yaml(co_yaml_path)
 co_dept_groups <- read.delim(co_dept_groups_path, sep="|")
-co_raw <- read_parquet(co_raw_path)
+co_raw <- read_csv(co_raw_path)
 co_regions <- read.delim(co_regions_path, sep=";") |>
     select(dept_code, muni_code, region_onic) |>
     rename(dept_code_hecho=dept_code, muni_code_hecho=muni_code)
